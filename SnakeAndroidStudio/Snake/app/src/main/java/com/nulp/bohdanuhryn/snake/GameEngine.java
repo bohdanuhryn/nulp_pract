82da694596479f25991affa3d380f2123105e7eb
bohdanuhryn <bohdanuhryn@gmail.com>
2015-06-25 00:42:45 +0300

    graphics update

--- SnakeAndroidStudio/Snake/app/src/main/java/com/nulp/bohdanuhryn/snake/GameEngine.java
+++ SnakeAndroidStudio/Snake/app/src/main/java/com/nulp/bohdanuhryn/snake/GameEngine.java
@@ -2,6 +2,10 @@ package com.nulp.bohdanuhryn.snake;
 
 import java.util.Vector;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.content.Intent;
 import android.graphics.Canvas;
@@ -10,11 +14,14 @@ import android.app.Activity;
 import android.os.Looper;
 import android.widget.TextView;
 import android.os.Handler;
+import android.content.Context;
 
 import com.nulp.bohdanuhryn.snake.gui.MainMenuActivity;
 import com.nulp.bohdanuhryn.snake.gui.SaveScoreActivity;
 
 public class GameEngine {
+
+    private Context context;
 
     private GameField gameField;
 
@@ -48,8 +55,9 @@ public class GameEngine {
         gameSpeed = snake.GetSpeed();
     }
 
-    public void InitActivity(final Activity context) {
-        final TextView score = (TextView) context.findViewById(R.id.score_text_view);
+    public void InitActivity(final Activity _context) {
+        context = _context;
+        final TextView score = (TextView) _context.findViewById(R.id.score_text_view);
         score.post(new Runnable() {
             public void run() {
                 String str = Integer.toString(snake.foodWeight).concat(" / ")
@@ -121,32 +129,102 @@ public class GameEngine {
         p.setARGB(255, 190, 190, 100);
         canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), p);
 
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.wall);
+
+        Rect srcRect = new Rect();
+        srcRect.top = 0;
+        srcRect.left = 0;
+        srcRect.bottom = bitmap.getHeight();
+        srcRect.right = bitmap.getWidth();
+
+        Rect destRect = new Rect();
+
         p.setARGB(255, 120, 80, 80);
         for(int i = 0; i < gameField.walls.size(); ++i) {
-            canvas.drawRect(
-                    gameField.walls.elementAt(i).x * cellSize,
-                    gameField.walls.elementAt(i).y * cellSize,
-                    (gameField.walls.elementAt(i).x + 1) * cellSize,
-                    (gameField.walls.elementAt(i).y + 1) * cellSize,
-                    p);
+            destRect.top = gameField.walls.elementAt(i).y * cellSize;
+            destRect.left = gameField.walls.elementAt(i).x * cellSize;
+            destRect.bottom = (gameField.walls.elementAt(i).y + 1) * cellSize;
+            destRect.right = (gameField.walls.elementAt(i).x + 1) * cellSize;
+            canvas.drawBitmap(bitmap, srcRect, destRect, p);
         }
 
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.food);
+        srcRect.top = 0;
+        srcRect.left = 0;
+        srcRect.bottom = bitmap.getHeight();
+        srcRect.right = bitmap.getWidth();
         p.setARGB(255, 255, 0, 0);
-        if(gameField.food != null)
-            canvas.drawCircle(
-                    gameField.food.x * cellSize + cellSize / 2,
-                    gameField.food.y * cellSize + cellSize / 2,
-                    cellSize / 2,
-                    p);
+        if(gameField.food != null) {
+            destRect.top = gameField.food.y * cellSize;
+            destRect.left = gameField.food.x * cellSize;
+            destRect.bottom = (gameField.food.y + 1) * cellSize;
+            destRect.right = (gameField.food.x + 1) * cellSize;
+            canvas.drawBitmap(bitmap, srcRect, destRect, p);
+        }
+
 
         p.setARGB(255, 0, 160, 0);
-        for(int i = 0; i < snake.body.size(); ++i) {
-            canvas.drawRect(
-                    snake.body.elementAt(i).x * cellSize,
-                    snake.body.elementAt(i).y * cellSize,
-                    (snake.body.elementAt(i).x + 1) * cellSize,
-                    (snake.body.elementAt(i).y + 1) * cellSize,
-                    p);
+        Matrix rotMatrix;
+        int snakeLength = snake.body.size();
+        for(int i = 0; i < snakeLength; ++i) {
+            rotMatrix = new Matrix();
+            if(i == 0 || i == snakeLength - 1) {
+                if(i == 0)
+                    bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.snake_head);
+                else
+                    bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.snake_tail);
+                srcRect.top = 0;
+                srcRect.left = 0;
+                srcRect.bottom = bitmap.getHeight();
+                srcRect.right = bitmap.getWidth();
+                int dh, dv;
+                if(i == 0) {
+                    dh = snake.body.elementAt(1).x - snake.body.elementAt(0).x;
+                    dv = snake.body.elementAt(1).y - snake.body.elementAt(0).y;
+                }
+                else {
+                    dh = snake.body.elementAt(i).x - snake.body.elementAt(i - 1).x;
+                    dv = snake.body.elementAt(i).y - snake.body.elementAt(i - 1).y;
+                }
+                if(dh == -1)
+                    rotMatrix.setRotate(90);
+                else if(dh == 1)
+                    rotMatrix.setRotate(270);
+                else if(dv == -1)
+                    rotMatrix.setRotate(180);
+            }
+            else {
+                int dhb, dha, dvb, dva;
+                dhb = snake.body.elementAt(i - 1).x - snake.body.elementAt(i).x;//horizontal delta
+                dvb = snake.body.elementAt(i).y - snake.body.elementAt(i - 1).y;//vertical delta
+                dha = snake.body.elementAt(i + 1).x - snake.body.elementAt(i).x;//horizontal delta
+                dva = snake.body.elementAt(i).y - snake.body.elementAt(i + 1).y;//vertical delta
+                if(dhb * dha + dvb * dva == 0) {// vactors are rectangular
+                    bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.snake_body_ang);
+                    if((dhb == -1 && dva == -1) || (dvb == -1 && dha == -1))
+                        rotMatrix.setRotate(90);
+                    else if((dhb == -1 && dva == 1) || (dvb == 1 && dha == -1))
+                        rotMatrix.setRotate(180);
+                    else if((dhb == 1 && dva == 1) || (dvb == 1 && dha == 1))
+                        rotMatrix.setRotate(270);
+                }
+                else {
+                    bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.snake_body);
+                    if(dhb == -1)
+                        rotMatrix.setRotate(90);
+                    else if(dhb == 1)
+                        rotMatrix.setRotate(270);
+                    else if(dvb == -1)
+                        rotMatrix.setRotate(180);
+                }
+            }
+            bitmap = Bitmap.createBitmap(
+                    bitmap, 0, 0, srcRect.right, srcRect.bottom, rotMatrix, false);
+            destRect.top = snake.body.elementAt(i).y * cellSize;
+            destRect.left = snake.body.elementAt(i).x * cellSize;
+            destRect.bottom = (snake.body.elementAt(i).y + 1) * cellSize;
+            destRect.right = (snake.body.elementAt(i).x + 1) * cellSize;
+            canvas.drawBitmap(bitmap, srcRect, destRect, p);
         }
     }
 
